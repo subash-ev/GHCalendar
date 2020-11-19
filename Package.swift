a907602dc244b2c27a5519a8779a9b772217fbf0
soltan <soltangh.work@gmail.com>
2020-11-20 00:28:51 +0200

    Modified package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,13 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "GHCalendar",
+            name: "GHCalendar-static",
+            type: .static,
             targets: ["GHCalendar"]),
+        .library(
+            name: "GHCalendar-dynamic",
+            type: .dynamic,
+            targets: ["GHCalendar"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
